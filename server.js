0743ec4f43c39ab786cad0a510d36af9ff37037f
jjtripode@hotmail.com <jjtripode@hotmail.com>
2024-04-21 10:59:12 -0300

    add ws webhooks

--- server.js
+++ server.js
@@ -9,6 +9,44 @@ const openai = new OpenAI({
 
 const app = express()
 
+app.get('/webhook', async (req,res)=>{
+  console.log(req);
+    if (
+        req.query['hub.mode'] == 'subscribe' &&
+        req.query['hub.verify_token'] == process.env.WHATSAPP_WEBHOOK_TOKEN
+    ) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.sendStatus(400);
+    }
+})
+
+// app.post("/webhook", (req, res) => {
+//   let body = req.body;
+
+//   if (req.body.object) {
+//     if (
+//       req.body.entry &&
+//       req.body.entry[0].changes &&
+//       req.body.entry[0].changes[0] &&
+//       req.body.entry[0].changes[0].value.messages &&
+//       req.body.entry[0].changes[0].value.messages[0]
+//     ) {
+
+//       // do your stuff here.....
+
+//       let phone_number_id =
+//         req.body.entry[0].changes[0].value.metadata.phone_number_id;
+//       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+//       let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+//     }
+//     res.sendStatus(200);
+//   } else {
+//     // Return a '404 Not Found' if event is not from a WhatsApp API
+//     res.sendStatus(404);
+//   }
+// });
+
 app.get('/', async (req,res)=>{
   res.end(JSON.stringify({ a: 'pppppcdsac' }));
 })
