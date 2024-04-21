1cdfb5924e5983f9f7a24ed55f54e2fdc307d680
jjtripode@hotmail.com <jjtripode@hotmail.com>
2024-04-21 12:57:22 -0300

    add ws webhooks

--- server.js
+++ server.js
@@ -68,6 +68,7 @@ app.post("/webhook", async (req, res) => {
 // accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
 // info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
 app.get("/webhook", (req, res) => {
+  console.log(req);
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
@@ -75,7 +76,7 @@ app.get("/webhook", (req, res) => {
   // check the mode and token sent are correct
   if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
     // respond with 200 OK and challenge token from the request
-    res.status(200).send(challenge);
+    res.status(200).send(req.query["hub.challenge"]);
     console.log("Webhook verified successfully!");
   } else {
     // respond with '403 Forbidden' if verify tokens do not match
