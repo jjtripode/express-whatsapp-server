7f9ad0bc549e106a6c8fcc4c0231697868f0eb6a
jjtripode@hotmail.com <jjtripode@hotmail.com>
2024-04-21 12:00:07 -0300

    add ws webhooks

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const OpenAI = require("openai");
-const express = require("express")
+const express = require("express");
+const body_parser=require("body-parser");
+const axios=require("axios");
 require('dotenv').config();
 
 console.log(`api es ${process.env.OPENAI_API_KEY}`)
+console.log(`api es ${process.env.WHATSAPP_WEBHOOK_TOKEN}`)
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -11,21 +15,26 @@ const app = express()
 
 //to verify the callback url from dashboard side - cloud api side
 app.get("/webhook",(req,res)=>{
+  console.log('entro al get');
   let mode=req.query["hub.mode"];
   let challenge=req.query["hub.challenge"];
   let token=req.query["hub.verify_token"];
-
-
+  console.log('lee request')
    if(mode && token){
 
        if(mode==="subscribe" && token===process.env.WHATSAPP_WEBHOOK_TOKEN){
-           res.status(200).send(challenge);
+   console.log('return 1')
+           
+        res.status(200).send(challenge);
        }else{
-           res.status(403);
+   console.log('return 2')
+
+           res.status(403).send();
        }
 
    }
-
+   console.log('return 3')
+   res.status(403).send();
 });
 
 app.post("/webhook",(req,res)=>{ //i want some 
@@ -48,10 +57,10 @@ app.post("/webhook",(req,res)=>{ //i want some
               console.log("phone number "+phon_no_id);
               console.log("from "+from);
               console.log("boady param "+msg_body);
-
+              let token='EAAU6Jo9BgUgBO4ZArt7Cr9BLAHC4H6jBezJq4vLAMNfZB8TOcgkkbkdGd8CIqqAs6XD9o0p83jjf6YoWqXt8Re3Q6GFY2wviUBy66r7NQrSjtGgZAzEKExxQ7H7AAWAYTENJgZCRWxCH9w6DBeM8UGZASBwQPWuZAKHgMJ2fnmdnWxZBclmVdCxGOpeJA4PJAsbTZAVDrTyfZCklB0yLv'
               axios({
                   method:"POST",
-                  url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                  url:"https://graph.facebook.com/v18.0/"+phon_no_id+"/messages?access_token="+token,
                   data:{
                       messaging_product:"whatsapp",
                       to:from,
