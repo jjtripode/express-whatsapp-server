c2b384e21b2c5d06c722bd5f661b231915d8e6eb
jjtripode@hotmail.com <jjtripode@hotmail.com>
2024-04-21 10:31:20 -0300

    agrego chatgpt pero no tengo credito

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+const OpenAI = require("openai");
 const express = require("express")
+require('dotenv').config();
+
+console.log(`api es ${process.env.OPENAI_API_KEY}`)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 const app = express()
 
-app.get('/', (req,res)=>{
-    res.end(JSON.stringify({ a: 'pppppcdsac' }));
+app.get('/', async (req,res)=>{
+  res.end(JSON.stringify({ a: 'pppppcdsac' }));
+})
+
+app.get('/chatgpt', async (req,res)=>{
+  const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "gpt-3.5-turbo",
+    });
+  
+  res.end(JSON.stringify({ a: 'pppppcdsac' }));
 })
 
 app.listen(3001)
