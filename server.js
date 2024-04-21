35bc8de77ae3ba5e590deb2d82514ebf5fe84e43
jjtripode@hotmail.com <jjtripode@hotmail.com>
2024-04-21 11:29:51 -0300

    add ws webhooks

--- server.js
+++ server.js
@@ -9,42 +9,69 @@ const openai = new OpenAI({
 
 const app = express()
 
-app.get('/webhook', async (req,res)=>{
-  console.log(req);
-    if (
-        req.query['hub.mode'] == 'subscribe' &&
-        req.query['hub.verify_token'] == process.env.WHATSAPP_WEBHOOK_TOKEN
-    ) {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.sendStatus(400);
-    }
-})
+//to verify the callback url from dashboard side - cloud api side
+app.get("/webhook",(req,res)=>{
+  let mode=req.query["hub.mode"];
+  let challenge=req.query["hub.challenge"];
+  let token=req.query["hub.verify_token"];
 
-app.post("/webhook", (req, res) => {
-  let body = req.body;
 
-  if (req.body.object) {
-    if (
-      req.body.entry &&
-      req.body.entry[0].changes &&
-      req.body.entry[0].changes[0] &&
-      req.body.entry[0].changes[0].value.messages &&
-      req.body.entry[0].changes[0].value.messages[0]
-    ) {
+   if(mode && token){
 
-      // do your stuff here.....
+       if(mode==="subscribe" && token===process.env.WHATSAPP_WEBHOOK_TOKEN){
+           res.status(200).send(challenge);
+       }else{
+           res.status(403);
+       }
 
-      let phone_number_id =
-        req.body.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-    }
-    res.sendStatus(200);
-  } else {
-    // Return a '404 Not Found' if event is not from a WhatsApp API
-    res.sendStatus(404);
-  }
+   }
+
+});
+
+app.post("/webhook",(req,res)=>{ //i want some 
+
+   let body_param=req.body;
+
+   console.log(JSON.stringify(body_param,null,2));
+
+   if(body_param.object){
+       console.log("inside body param");
+       if(body_param.entry && 
+           body_param.entry[0].changes && 
+           body_param.entry[0].changes[0].value.messages && 
+           body_param.entry[0].changes[0].value.messages[0]  
+           ){
+              let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+              let from = body_param.entry[0].changes[0].value.messages[0].from; 
+              let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+              console.log("phone number "+phon_no_id);
+              console.log("from "+from);
+              console.log("boady param "+msg_body);
+
+              axios({
+                  method:"POST",
+                  url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                  data:{
+                      messaging_product:"whatsapp",
+                      to:from,
+                      text:{
+                          body:"Hi.. I'm Prasath, your message is "+msg_body
+                      }
+                  },
+                  headers:{
+                      "Content-Type":"application/json"
+                  }
+
+              });
+
+              res.sendStatus(200);
+           }else{
+               res.sendStatus(404);
+           }
+
+   }
+
 });
 
 app.get('/', async (req,res)=>{
